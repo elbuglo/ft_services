e34712090de5500edb4a67fc8abe182ce622c62e
lulebugl <lulebugl@student.42.fr>
2020-04-16 14:53:22 +0200

    grafna marche enfin, passons a la suite

---
+++ srcs/ftps/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+apk upgrade
+apk add openrc openssl --no-cache
+apk add pure-ftpd --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted --no-cache
+
+# ssl
+yes "" | openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+-subj '/C=FR/ST=75/L=Paris/O=42/CN=lulebugl' \
+-keyout /etc/ssl/private/pure-ftpd.pem -out /etc/ssl/private/pure-ftpd.pem 
+
+adduser -D "admin"
+echo "admin:admin" | chpasswd
+
+openrc
+touch /run/openrc/softlevel
