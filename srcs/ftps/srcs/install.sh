5a71e73d3a86987e13b4198e2257caf0c680af38
lulebugl <lulebugl@student.42.fr>
2020-04-16 13:18:42 +0200

    grafna marche enfin, passons a la suite

--- srcs/ftps/srcs/install.sh
+++ srcs/ftps/srcs/install.sh
@@ -2,7 +2,7 @@
 
 apk upgrade
 apk add openrc openssl --no-cache
-apk add telegraf pure-ftpd --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted --no-cache
+apk add pure-ftpd --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted --no-cache
 
 # ssl
 yes "" | openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
@@ -14,4 +14,3 @@ echo "nine:nine" | chpasswd
 
 openrc
 touch /run/openrc/softlevel
-rc-update add telegraf
